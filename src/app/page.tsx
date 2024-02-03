b3663801fb2db2b2f21810d5e7cb8f8a6bcf8b3d
Irad Doron <irad.doron@gmail.com>
2024-02-03 21:21:17 +0200

    add activity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,56 @@ export default function Home() {
 		<div>
 			<h1>Drubakuna</h1>
 			<h2>Basic activities</h2>
-			<h3>Drinking water</h3>
-			<p>
-				<span>How to drink water:</span> take a glass, fill it with cold water
-				from the water bar device, and drink it.
-			</p>
-			<p>
-				<span>When to drink water:</span> Drink water at the following
-				scenarios:
-			</p>
-			<ul>
-				<li>When you feel thirsty</li>
-			</ul>
-			<p>Notes:</p>
-			<ul>
-				<li>Use only one glass. If you need more water, refill the glass.</li>
-			</ul>
+			<section>
+				<h3>Drinking water</h3>
+				<p>
+					<span>How to drink water:</span> take a glass, fill it with cold water
+					from the water bar device, and drink it.
+				</p>
+				<p>
+					<span>When to drink water:</span> Drink water at the following
+					scenarios:
+				</p>
+				<ul>
+					<li>When you feel thirsty</li>
+				</ul>
+				<p>Notes:</p>
+				<ul>
+					<li>Use only one glass. If you need more water, refill the glass.</li>
+				</ul>
+			</section>
+			<section>
+				<h3>Cleaning dishes</h3>
+				<p>
+					<span>How to clean dishes:</span> take a dirty dish, put it under the
+					sink and clean it with water and soap. Then, rinse it with water and
+					put it in the dish rack. When the dish rack is full, dry the dishes
+					and put them in thier respective places. If there are dry dishes in
+					the dish rack, put them in thier respective places.
+				</p>
+				<p>
+					<span>When to clean dishes:</span> Clean dishes at the following
+					scenarios:
+				</p>
+				<ul>
+					<li>When you finish eating</li>
+					<li>When you finish cooking</li>
+				</ul>
+				<p>Notes:</p>
+				<ul>
+					<li>
+						If you finished cooking and you need to eat, clean the dishes after
+						you finish eating. If it possible, clean as much dishes as you can
+						before you start eating. Good and warm meal is more importent than
+						cleaning the dishes right away.
+					</li>
+					<li>
+						At the moment you finished eating, clean the dishes right away. If
+						you are not able to clean the dishes right away, put them in the
+						sink.
+					</li>
+				</ul>
+			</section>
 		</div>
 	);
 }
